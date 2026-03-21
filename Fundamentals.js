d18cd57dd2ac9b555ca5ab2c5274c262202c7e77
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-03-21 11:43:54 +0100

    Update Fundamentals.js

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,3 +1,111 @@
 /*
 ****** Build a strong foundation with essential concepts and basic skills
 */
+
+```js
+// Type your code below
+let k = 88;
+let PI = 3.14;
+let name = "bob";
+
+// Don't change the line below
+console.log(`k = ${k}`)
+console.log(`PI = ${PI}`)
+console.log(`name = ${name}`)
+```
+
+```js
+// Type your code below
+const minSize = 50;
+
+// Don't change the line below
+console.log(`minSize = ${minSize}`)
+```
+
+```js
+// Type your code below
+const maxSpeed = 120;
+
+// Don't change the line below
+console.log(`maxSpeed = ${maxSpeed}`)
+```
+
+```js
+// Type your code below
+let a = 5.2;
+let b = 2.6;
+let c = a / b;
+
+// Don't change the line below
+console.log(`a = ${a}, b = ${b}, c = ${c}`);
+```
+
+```js
+// Type your code below
+let x = 12;
+let y = 3;
+let z = x * y;
+
+// Don't change the line below
+console.log(`x = ${x}, y = ${y}, z = ${z}`);
+```
+
+```js
+// Type your code below
+let a = 9;
+let b = 2.6;
+let c = 11;
+let d = a % 2;
+let e = a % 3;
+let f = c % 10;
+
+// Don't change the line below
+console.log(`a = ${a}`)
+console.log(`b = ${b}`)
+console.log(`c = ${c}`)
+console.log(`d = ${d}`)
+console.log(`e = ${e}`)
+console.log(`f = ${f}`)
+```
+
+```js
+// Type your code below
+let x = 15;
+let y = 4;
+let z = x % y;
+
+// Don't change the line below
+console.log(`x = ${x}, y = ${y}, z = ${z}`)
+```
+
+```js
+// Type your code below
+let count = 0
+count = 4;
+count *= 2;
+count -= 1;
+
+// Don't change the line below
+console.log(`count = ${count}`)
+```
+
+```js
+// Type your code below
+let balance = 0
+balance += 100;
+balance *= 1.10;
+balance -= 50;
+
+// Don't change the line below
+console.log(`balance = ${Math.floor(balance)}`)
+```
+
+```js
+// Type your code below
+let n1 = 8;
+let n2 = 9;
+let n3 = n1 > n2;
+
+// Don't change the line below
+console.log(`n1 = ${n1}, n2 = ${n2}, n3 = ${n3}`)
+```
