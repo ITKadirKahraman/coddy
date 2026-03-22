bc527cef7deb27f1efefd370209001f7a342e4a4
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-03-22 14:31:58 +0100

    Update Fundamentals.js

--- Fundamentals.js
+++ Fundamentals.js
@@ -146,3 +146,12 @@ let b = 4;
 let c = b * a
 console.log(`c = ${c}`)
 ```
+```js
+// Type your code below
+let b1 = true;
+let b2 = false;
+let b3 = b1 || b2
+
+// Don't change the line below
+console.log(`b3 = ${b3}`)
+```
