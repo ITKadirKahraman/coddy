ce9b9629275f2c8c5e5047e49cbfb9279bb143bb
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-04-15 19:36:25 +0200

    Create einfuehrung.js

---
+++ Fundamentals/einfuehrung.js
@@ -0,0 +1,20 @@
+// DIE SPRACHE
+
+/*
+JavaScript ist eine der weltweit beliebtesten Programmiersprachen, 
+die primär für die Webentwicklung verwendet wird.
+*/
+
+// AUSGABE
+console.log("Hello World!");
+
+// KOMMENTARE 
+// /**/
+
+/*
+CODESTRUKTUR 
+
+JavaScript Code wird von oben nach unten, Zeile für Zeile ausgeführt.
+Jede Codezeile ist ein Befehl (auch Anweisung genannt), der dem Computer sagt, was er tun soll.
+Semikolen ist kein muss, ist aber eine gute Gewohnheit, um Fehler zu vermeiden!
+*/
