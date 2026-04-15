f4861270076bf9c81f58f863ea06771c56c2aa9c
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-04-15 20:04:18 +0200

    Create variablen.js

---
+++ Fundamentals/variablen.js
@@ -0,0 +1,65 @@
+// Variablen 
+
+// Variablen sind Datenspeicher mit denen wir arbeiten können
+
+let number = 10;
+
+// Challange
+// Variable erstllen mit einem Wert initialisieren
+let a = 5;
+console.log(`Die Variable a hat eine initalisierten Wert von: ${a}`);
+
+// Challange
+// Schreibe Code, der eine Variable namens x mit dem Wert 3.14 initialisiert.
+let x = 3.14;
+console.log(`Die Variable a hat eine initalisierten Wert von: ${x}`);
+
+// Challange
+// Speichere den String I am learning to code with Coddy! in einer Variable namens coddy.
+let coddy = "I am learning to code with Coddy!";
+console.log(`${coddy}`);
+
+// Challange
+// Speichere den String Hello, welcome to JavaScript! in einer Variable namens greeting.
+let greeting = "Hello, welcome to JavaScript!";
+console.log(`${greeting}`);
+
+// Challange
+// Deklariere eine Variable namens boolean und weise ihr den Wert true zu.
+let boolean = true;
+console.log(`${boolean}`);
+
+// Challenge
+// Deklariere eine Variable namens isLoggedIn und weise ihr den Wert false zu, um darzustellen, dass ein Benutzer derzeit nicht angemeldet ist.
+let isLoggedIn = false;
+console.log(`${isLoggedIn}`);
+
+// Challenge
+// Erstelle zwei Variablen: firstName und weise ihr den Wert "John" zu lastName und weise ihr den Wert "Doe" zu Befolge die JavaScript-Namenskonventionen.
+let firstName = "John";
+let lastName = "Doe";
+console.log(`${firstName+ " " +lastName}`);
+console.log(`${firstName}, ${lastName}`);
+
+// Challenge
+// Deklariere zwei leere Variablen namens a und b.
+let aa = "";
+let bb= "";
+console.log(`${aa}, ${bb}`);
+
+// Challenge
+// Schreibe Code, der die folgenden Variablen initialisiert: k mit dem Wert 88, PI mit dem Wert 3.14, name mit dem Wert "bob"
+
+let k = 88;
+let pi = 3.14;
+let name = "bob";
+
+// Challenge
+// Initialisiere eine konstante Variable namens minSize mit dem Wert 50
+minSize = 50;
+console.log(`${minSize}`);
+
+// Challenge
+// Initialisiere eine konstante Variable namens maxSpeed mit dem Wert 120
+const maxSpeed = 120;
+console.log(`${maxSpeed}`);
